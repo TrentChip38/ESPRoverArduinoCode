d25bec77f9e6dfa9f29e74f061ff5a1132ca8853
Trent Chipman <trentchip@gmail.com>
2026-04-15 14:14:48 -0600

    Seeed launch logic mostly works, but adding improvements

--- SeedBLE/SeedBLE.ino
+++ SeedBLE/SeedBLE.ino
@@ -10,17 +10,33 @@ BLEUart bleuart;
 //Instantiate the IMU
 LSM6DS3 imu(I2C_MODE, 0x6A);
 //Sample rate and thresholds
-const float LAUNCH_THRESHOLD = 8.0; // g
-const float LANDING_THRESHOLD = 1.05; // g
+const float LAUNCH_THRESHOLD = 4.0; // g
+const float LAUNCH_THRESHOLD_TIME = 1000; // 1000 ms = 1 sec
+const float LANDING_THRESHOLD_HIGH = 1.1; // g
+const float LANDING_THRESHOLD_LOW = 0.95; // g
+const int LANDING_COUNT = 100; // a couple seconds?
+int land_counter = 0;
+const float alpha = 0.05;
+float avgMag = 0.0;
 const int sampleRateHz = 50;
+unsigned long startTime = 0;
+unsigned long currentTime = 0;
+unsigned long elapsedTime = 0;
+
 //ENUM for STATE
 enum SeedState {
   WAITING_FOR_LAUNCH,
+  LAUNCHING,
   IN_FLIGHT,
   LANDING,
-  LANDED
+  LANDED,
+  OPEN,
+  DEPLOY
 };
 SeedState STATE = WAITING_FOR_LAUNCH;
+//4g for 2 sec
+//Wait 10 min
+//Check not less than 1g (free fall)
 
 // Servo doorServo;
 
@@ -39,14 +55,18 @@ void setup() {
   // doorServo.write(0); // door closed
   //Set up BLE advertised data
   Bluefruit.begin();
-  Bluefruit.setTxPower(4);
+  //Bluefruit.setTxPower(4);
   Bluefruit.setName("RocketDoor");
 
   bleuart.begin();
 
   Bluefruit.Advertising.addFlags(BLE_GAP_ADV_FLAGS_LE_ONLY_GENERAL_DISC_MODE);
+  Bluefruit.Advertising.addTxPower();
   Bluefruit.Advertising.addService(bleuart);
-  Bluefruit.Advertising.start();
+  //Bluefruit.Advertising.start();
+  Bluefruit.ScanResponse.addName();   // optional but helpful
+
+  Bluefruit.Advertising.start(0);     // advertise forever
 }
 
 void openDoorAndSignal() {
@@ -64,42 +84,79 @@ float readIMU() {
 }
 
 void loop() {
-  bleuart.println("Hello from rocket!");
-  delay(1000);
+  //Get current time
+  // Serial.print("Elapsed Time (ms): ");
+  // Serial.println(elapsedTime);
+
+  // bleuart.println("Hello from rocket!");
+  //delay(1000);
   // Read IMU accelaration magnitude
   float accel = readIMU();
+  // Rolling average (low-pass filter)
+  avgMag = alpha * accel + (1 - alpha) * avgMag;
 
   // Serial output
   Serial.print("Current: ");
   Serial.print(accel, 3);
+  Serial.print(" : ");
+  Serial.print(avgMag, 3);
   Serial.print(" g, State: ");
   Serial.println(STATE);
 
   //State machine (Wait, in flight, landing, landed)
   if (STATE == WAITING_FOR_LAUNCH) {
-      if (accel > LAUNCH_THRESHOLD) {
-          STATE = IN_FLIGHT;
-          //Start timer?
+    bleuart.println("WAITING");
+      if (avgMag > LAUNCH_THRESHOLD) {
+          STATE = LAUNCHING;
+          startTime = millis(); // Record starting time
       }
+  }else if (STATE == LAUNCHING) {
+    bleuart.println("LAUNCHING");
+    STATE = IN_FLIGHT;
+    currentTime = millis();
+    elapsedTime = currentTime - startTime;
+    //If 1 second has elapsed since first 4g
+    if (elapsedTime >= LAUNCH_THRESHOLD_TIME){
+      //And acceleration is still > 4 g
+      if (avgMag > LAUNCH_THRESHOLD){
+        //Then its launching
+        STATE = IN_FLIGHT;
+        //Restart timer for next timing?
+      }else{
+        //Its not really launching
+        STATE = WAITING_FOR_LAUNCH;
+      }
+    }
   }else if (STATE == IN_FLIGHT) {
+    bleuart.println("FLIGHT");
       delay(1000 * 10); // wait for min duration of flight
+      //Flight should be 7 min. We will wait 10 min.
       STATE = LANDING;
-      // if (steady_low_accel over T seconds) {
-      //     STATE = LANDED;
-      //     start_timer();
-      // }
+      land_counter = 0;
   }else if (STATE == LANDING) {
-      if (accel < LANDING_THRESHOLD) {
-          //If not moving for like 10 seconds, consider it landed
+    bleuart.println("LANDING");
+      if (accel < LANDING_THRESHOLD_HIGH && accel > LANDING_THRESHOLD_LOW) {
+        land_counter += 1;
+        //If not moving or in free fall for a few seconds, consider it landed
+        if (land_counter >= LANDING_COUNT){
           STATE = LANDED;
+        }
       }
   }else if (STATE == LANDED) {
+    bleuart.println("LANDED");
       // if (timer_expired()) {
       //     open_door();
       //     send_rover_signal();
       //     break;
       // }
       delay(6 * 1000);   // 60 s post-landing delay
+      STATE = DEPLOY;
+      landed = true;
+  }else if (STATE == OPEN) {
+    bleuart.println("OPEN");
+      landed = true;//Open door then wait
+  }else if (STATE == DEPLOY) {
+    bleuart.println("DEPLOY");
       landed = true;
   }
 
